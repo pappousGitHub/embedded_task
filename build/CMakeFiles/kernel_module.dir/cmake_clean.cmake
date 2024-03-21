2efd891f4318b2b2218ae110a5c318459c3529bb
spyros <pappous19@gmail.com>
2024-03-21 18:10:55 +0000

    add build folder

---
+++ build/CMakeFiles/kernel_module.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kernel_module.dir/kernel_module.c.o"
+  "out/kernel_module.ko"
+  "out/kernel_module.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kernel_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
